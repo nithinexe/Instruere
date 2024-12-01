56b56af4f22fa950d828d849571510a3b4490a1f
Sahil <sahil2215ali@gmail.com>
2024-12-02 04:46:44 +0530

    refactor

---
+++ frontend/src/app/upload/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UploadForm from '../components/UploadForm'
+
+const Page = () => {
+  return (
+    <div>
+      <UploadForm />
+    </div>
+  )
+}
+
+export default Page
